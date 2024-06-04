8fed2d89cdb011594f495a8bc433db3bab927720
Yago Pacotinho <YagoPacotinho@Leticias-MacBook-Pro.local>
2024-06-04 16:09:28 +0100

    Separated Barcode Scanner feature from Camera

---
+++ src/app/(features)/barcode/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MarkdownDisplay from "../../../components/MarkdownDisplay";
+
+const description = `
+# Barcode Scanner
+Scan barcode from products using Reactive Native Vision Camera.`;
+
+const FeatureDetailsScreen = () => {
+  return (
+    <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: "Barcode" }} />
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
+      <Link href="/barcode/scanner" asChild>
+        <Button title="Go to Barcode Scanner" />
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default FeatureDetailsScreen;
